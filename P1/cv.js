7880ef8c15213cb7eb0711d3c1841ac934a8e671
dsota <d.sota.2020@alumnos.urjc.es>
2024-02-29 22:39:14 +0100

    let me see if that works

---
+++ P1/cv.js
@@ -0,0 +1,6 @@
+function secret_key() {
+    document.body.style.backgroundImage = "url('_secret.jpeg')";
+    var canvas = document.getElementsByClassName("canvas")[0];
+    canvas.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    
+}
