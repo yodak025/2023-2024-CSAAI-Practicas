6f7aee0d4ca149087ed5653868d03dfccee6871c
dsota <d.sota.2020@alumnos.urjc.es>
2024-03-15 00:26:35 +0100

    life could be a dream

---
+++ P2/MasterMindGame.js
@@ -0,0 +1,79 @@
+class MasterMind{
+    
+
+    constructor(alph){
+
+        this.ALPHABET = alph
+        this.LEN = 4
+        this.KEY = this._random_key()
+
+        this.turns = 10
+        this.curr_el = 0
+        
+
+
+
+    }
+
+    _random_key(){
+        let key = []
+        for(let i = 0; i < this.LEN; i++){
+            key.push(Math.floor(Math.random() * this.ALPHABET.length))
+        }
+        return new Int8Array(key)
+    }
+
+    _matches(num){
+        if (num == this.KEY[this.curr_el]) {
+            this.curr_el += 1
+            return true
+        } else {
+            for (let i = 0; i < this.KEY.length; i++) {
+                if (this.KEY[i] == num) {
+                    this.curr_el += 1
+                    return false
+                }
+            }
+            this.curr_el += 1
+            return null
+        }
+
+            
+    }
+
+    _translator(x){ 
+        for (let i = 0; i < this.ALPHABET.length; i++){
+            if (this.ALPHABET[i] == x) {
+                return i 
+            }
+        }
+        alert("Error in translation")
+        return null
+    }
+
+    game(symb){
+        let num = this._translator(symb)
+        if (this.curr_el == this.LEN) {
+            this.curr_el = -1
+            this.turns -= 1
+            if (this.turns == 0) {
+                console.alert("Game over")
+            } 
+
+        } 
+        if (this._matches(num) == null){
+            return "*"
+        } else if (this._matches(num) == true){
+            return "+"
+        } else {
+            return "-"
+        }
+        
+
+    }
+
+
+    
+}
+
+
--- P2/boom.js
+++ P2/boom.js
@@ -1,32 +1,29 @@
-//boom = GameController("0123456789", 4, 10);
 
-//boom.new_turn("0101")
 
 
 
 function init() {
-    var buttons = []
+    alph = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    var count = 0
+    var MMG = new MasterMind(alph)
+
+    var buttons = []
     var displays = []
+
     for (let i = 0; i < 4; i++) {
-        displays[i] = document.getElementById("d" + String(i));
-        
+        displays[i] = document.getElementById("d" + String(i));      
     }
+
 
     for (let i = 0; i < 10; i++) {
         buttons[i] = document.getElementById(String(i));;
         buttons[i].onclick = () => {
-            displays[count].innerHTML = String(i);
-            count += 1;
+            input = buttons[i].querySelector("h1").textContent
+            displays[MMG.curr_el].innerHTML = MMG.game(input);
+            
         }
     }
-    
-    displays = []
-    for (let i = 0; i < 4; i++) {
-        displays[i] = document.getElementById("d" + String(i));
-        
-    }
+
 
 }
 
