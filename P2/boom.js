04bd4f02c8a956edae532d7585486d694c8a9c30
dsota <d.sota.2020@alumnos.urjc.es>
2024-03-15 13:16:23 +0100

    live could be a dream[chrono]

--- P2/boom.js
+++ P2/boom.js
@@ -6,6 +6,7 @@ function init() {
     alph = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     var MMG = new MasterMind(alph)
+    var chrono = new Crono(document.getElementsByClassName("chrono")[0]);
 
     var buttons = []
     var displays = []
@@ -15,6 +16,25 @@ function init() {
     }
 
 
+    var start = document.getElementById("Start");
+    var stop = document.getElementById("Stop");
+    var reset = document.getElementById("Reset");
+
+
+    start.onclick = () => {
+        chrono.start();
+    }
+
+
+    stop.onclick = () => {
+        chrono.stop();
+    }
+
+    reset.onclick = () => {
+        chrono.reset();
+    }
+
+    
     for (let i = 0; i < 10; i++) {
         buttons[i] = document.getElementById(String(i));;
         buttons[i].onclick = () => {
