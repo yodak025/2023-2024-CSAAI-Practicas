50e2247512d71d8b213431660da2f7cf50a0c3e1
dsota <d.sota.2020@alumnos.urjc.es>
2024-03-21 21:33:01 +0100

    life could be a dream ...

--- P2/boom.js
+++ P2/boom.js
@@ -5,7 +5,7 @@
 function init() {
     alph = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-    var MMG = new Game(alph)
+    var boom = new Game(alph)
     var chrono = new Crono(document.getElementsByClassName("chrono")[0]);
 
     var started = false;
@@ -41,9 +41,16 @@ function init() {
     reset.onclick = () => {
         chrono.stop();
         chrono.reset();
-        MMG = new Game(alph)
+        boom = new Game(alph)
         document.getElementsByClassName("selected")[0].classList.remove("selected");
         displays[0].classList.add("selected");
+        for (let n = 0; n < 4; n++) {
+            displays[n].innerHTML = "*";
+            displays[n].classList.remove("mistake");
+            displays[n].classList.remove("match");
+            displays[n].classList.remove("demimatch");
+        }
+        
     }
 
 
@@ -57,12 +64,12 @@ function init() {
 
             if (started) {
                 let pointer = false
-                for (let j = MMG.curr_el; !pointer; j++) {
+                for (let j = boom.curr_el; !pointer; j++) {
 
-                    if (j >= MMG.LEN - 1) {
-                        j -= MMG.LEN;
+                    if (j >= boom.LEN - 1) {
+                        j -= boom.LEN;
                     }
-                    if (MMG.key[j + 1] != -1) {
+                    if (boom.key[j + 1] != -1) {
                         document.getElementsByClassName("selected")[0].classList.remove("selected");
                         displays[j + 1].classList.add("selected");
                         pointer = true;
@@ -72,39 +79,43 @@ function init() {
 
                 TurnProcessor(i);
 
-                MMG.finish()
+                boom.finish()
+
+                if (all_el(boom.key, -1)) {
+                    window.alert("You win!")
+                }
             }
         }
 
     }
 
     function TurnProcessor(x) {
-        switch (MMG.game(x)) {
+        switch (boom.game(x)) {
 
             case "++":
-                MMG.finish()
+                boom.finish()
                 TurnProcessor(x);
                 break;
 
             case "*":
-                displays[MMG.curr_el].innerHTML = "*";
-                displays[MMG.curr_el].classList.add("mistake");
-                displays[MMG.curr_el].classList.remove("match");
-                displays[MMG.curr_el].classList.remove("demimatch");
+                displays[boom.curr_el].innerHTML = "*";
+                displays[boom.curr_el].classList.add("mistake");
+                displays[boom.curr_el].classList.remove("match");
+                displays[boom.curr_el].classList.remove("demimatch");
                 break;
 
             case "-":
-                displays[MMG.curr_el].innerHTML = "-";
-                displays[MMG.curr_el].classList.add("demimatch");
-                displays[MMG.curr_el].classList.remove("match");
-                displays[MMG.curr_el].classList.remove("mistake");
+                displays[boom.curr_el].innerHTML = "-";
+                displays[boom.curr_el].classList.add("demimatch");
+                displays[boom.curr_el].classList.remove("match");
+                displays[boom.curr_el].classList.remove("mistake");
                 break;
 
             case "+":
-                displays[MMG.curr_el].innerHTML = x;
-                displays[MMG.curr_el].classList.add("match");
-                displays[MMG.curr_el].classList.remove("mistake");
-                displays[MMG.curr_el].classList.remove("demimatch");
+                displays[boom.curr_el].innerHTML = x;
+                displays[boom.curr_el].classList.add("match");
+                displays[boom.curr_el].classList.remove("mistake");
+                displays[boom.curr_el].classList.remove("demimatch");
                 break;
         }
     }
@@ -112,4 +123,16 @@ function init() {
 
 }
 
+function all_el (arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]!= val) {
+            return false
+        }
+    }
+    return true
+}
+
+function real_init() {
+    init();
+}
 
